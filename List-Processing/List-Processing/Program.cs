82bb02c60ea07b5dc497b6814785e83e36ee670b
Martin Petkov <snutz@mail.bg>
2017-11-12 11:31:27 +0200

    Initial Commit

---
+++ List-Processing/List-Processing/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace List_Processing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
