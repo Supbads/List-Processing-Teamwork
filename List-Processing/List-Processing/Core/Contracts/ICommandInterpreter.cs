8abd229da8a4c99ca77bc654981bfd04d1ff8b69
b0yank <bnkaloyanov@gmail.com>
2017-11-13 13:16:05 +0200

    Added Reverse and Insert commands

---
+++ List-Processing/List-Processing/Core/Contracts/ICommandInterpreter.cs
@@ -0,0 +1,9 @@
+﻿namespace List_Processing.Core.Contracts
+{
+    using List_Processing.Core.Models.Commands;
+
+    public interface ICommandInterpreter
+    {
+        Command ParseCommand(string commandInput);
+    }
+}
---
+++ List-Processing/List-Processing/Core/Models/Commands/EndCommand.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace List_Processing.Core.Models.Commands
+{
+    public class EndCommand : Command
+    {
+        public EndCommand(IList<string> parameters)
+            : base(parameters)
+        {
+        }
+
+        public override void Execute(Data data)
+        {
+            data.EndReceived = true;
+        }
+    }
+}
